995e44bfc5cf767fb9998599427e4a1f6c2d503e
Eric Chapdelaine <emchap4@gmail.com>
2021-08-29 00:39:04 -0400

    Add starting node and sort by weight

--- app.py
+++ app.py
@@ -11,41 +11,53 @@ from graphs import (
 
 app = Flask(__name__)
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route('/')
 def index():
+    priority = sorted(all_units, key=lambda x: x.getDaysLeft())
+    return render_template("index.html", units=priority)
+
+@app.route("/create-path/<start>", methods=['GET', 'POST'])
+def createPath(start):
+    start_node = map_of_name[start]
+    available_nodes = [node for node in all_units if node != start_node]
     if request.method == 'POST':
         requested_nodes = list(map(lambda x: map_of_name[x], request.form.getlist('check')))
+        requested_nodes.append(start_node)
         if (len(requested_nodes) < 2):
             time = 0
             path = ["No path necessary"]
         else:
-            graph = Graph(requested_nodes, edges)
-            min_spanning = graph.create_min_spanning_tree()
-
-            time = graph.get_total_time(min_spanning)
-            path = graph.optimize_path(min_spanning)
+            graph = Graph(requested_nodes, distance_matrix)
+            path = graph.optimize_path()
+            time = graph.get_total_time()
             if (len(path) != len(requested_nodes) - 1):
                 time = 0
                 path = ["Could not complete path"]
-        
-        return render_template('index.html', units=all_units, checked=requested_nodes, time=time, path=path)
-    return render_template('index.html', units=all_units, checked=[], time=0, path=["Press the Update Path button for a solution."])
+        addition_of_others = {node: Graph([node] + requested_nodes, distance_matrix).get_total_time()-time for node in available_nodes}
+    else:
+        addition_of_others = {node: Graph([node, start_node], distance_matrix).get_total_time() for node in available_nodes}
+        time = 0
+        path=["Press the Update Path button for a solution."]
+        requested_nodes = []
+    sorted_by_distance = sorted(available_nodes, key=lambda x: addition_of_others[x]*x.getDaysLeft())
+    return render_template('create-path.html', time_others=addition_of_others, start=start_node, units=sorted_by_distance, checked=requested_nodes, time=time, path=path)
+
 
 if __name__ == "__main__":
-    v1 = Vertex("Location 1")
-    v2 = Vertex("Location 2")
-    v3 = Vertex("Location 3")
-    v4 = Vertex("Location 4")
-    v5 = Vertex("Location 5")
-    v6 = Vertex("Location 6")
+    v1 = Vertex("Location 1", 5)
+    v2 = Vertex("Location 2", 15)
+    v3 = Vertex("Location 3", 20)
+    v4 = Vertex("Location 4", 3)
+    v5 = Vertex("Location 5", 7)
+    v6 = Vertex("Location 6", 8)
     all_units = [v1, v2, v3, v4, v5, v6]
     map_of_name = {node.getName(): node for node in all_units}
-    edges = {
-        v1: {v2: 2, v3: 1, v4: 7, v5: 3, v6: 6},
-        v2: {v3:4, v4: 5, v5: 4, v6: 7},
-        v3: {v4: 6, v5: 2, v6: 8},
-        v4: {v5: 11, v6: 5},
-        v5: {v6: 2},
+    distance_matrix = {
+        v1: {v2: 1, v3: 4, v4: 1, v5: 3, v6: 6},
+        v2: {v3:3, v4: 2, v5: 2, v6: 7},
+        v3: {v4: 3, v5: 2, v6: 6},
+        v4: {v5: 1, v6: 6},
+        v5: {v6: 5},
         v6: {}
     }
 
--- graphs.py
+++ graphs.py
@@ -1,11 +1,11 @@
 class Edge:
-    def __init__(self, start, end, weight):
+    def __init__(self, start, end, time):
         self.start = start
         self.end = end
-        self.weight = weight
-
-    def getWeight(self):
-        return self.weight
+        self.time = time 
+    
+    def getTime(self):
+        return self.time
 
     def getStart(self):
         return self.start
@@ -14,14 +14,15 @@ class Edge:
         return self.end
 
     def __repr__(self):
-        return "Go from " + self.start.getName() + " to " + self.end.getName() + " (takes about " + str(self.weight) + ")."
+                return "Go from " + self.start.getName() + " to " + self.end.getName() + " (takes about " + str(self.getTime()) + ")."
 
 # These are the machines (or the Nodes in the Graph). 
 # You can add whatever fields/methods
 class Vertex:
-    def __init__(self, name):
+    def __init__(self, name, days_left):
         # For the use in Flask, these names must be unique
         self.name = name
+        self.days_left = days_left
 
     def __repr__(self):
         return self.name
@@ -29,44 +30,38 @@ class Vertex:
     def getName(self):
         return self.name
 
+    def getDaysLeft(self):
+        return self.days_left
+
 class Graph:
     def __init__(self, nodes, edges):
-        self.edges = {}
+        self.edges = {node: [] for node in nodes}
         self.list_of_edges = []
         for fromVertex in nodes:
-            cur = {}
             for toVertex in edges[fromVertex]:
                 if (toVertex in nodes):
-                    curWeight = edges[fromVertex][toVertex]
-                    cur[toVertex] = curWeight 
-                    self.list_of_edges.append(Edge(fromVertex, toVertex, curWeight))
-            self.edges[fromVertex] = cur
+                    self.edges[fromVertex].append(Edge(fromVertex, toVertex, edges[fromVertex][toVertex]))
+                    self.list_of_edges.append(Edge(fromVertex, toVertex, edges[fromVertex][toVertex]))
+
+                    self.edges[toVertex].append(Edge(toVertex, fromVertex, edges[fromVertex][toVertex]))
+                    self.list_of_edges.append(Edge(toVertex, toVertex, edges[fromVertex][toVertex]))
 
         self.nodes = nodes
-
-    def add_edge(self, node, edge):
-        self.list_of_edges.append(edge)
-        self.edges[node].append(edge)
-
-    def add_node(self, node, dic):
-        self.nodes.append(node)
-        self.edges[node] = []
-        for other in dic:
-            e  = Edge(node, other, dic[other])
-            self.edges[node].append(e)
-            self.list_of_edges.append(e)
+        self.min_tree = self.kruskals()
 
     def find(self, reps, node):
         if (reps[node] == node):
             return node
         else:
             return self.find(reps, reps[node])
-    
 
-    # Uses a modified version of Kruskal's algorithm to generate minimum spanning tree with no
-    # nodes having two (or more) edges
-    def create_min_spanning_tree(self):
-        worklist = sorted(self.list_of_edges, key=lambda i: i.getWeight()) 
+    # TODO: The below algorithm is okay, but it would be a lot better if you used a modified k-opt approach. 
+    # (I didn't have time to implement it, however).
+
+    # Creates a min spanning tree according to a modified Kruskals algorithm that doesn't allow for
+    # one edge to have more than 2 edges.
+    def kruskals(self):
+        worklist = sorted(self.list_of_edges, key=lambda i: i.getTime()) 
         output = {node: [] for node in self.nodes}
 
         reps = {node: node for node in self.nodes} 
@@ -81,38 +76,56 @@ class Graph:
             if (not creates_cycle and edge < 2):
                 output[cur.getStart()].append(cur)
                 # Assume that A->B takes the same amount of time as B->A
-                output[cur.getEnd()].append(Edge(cur.getEnd(), cur.getStart(), cur.getWeight()))
+                output[cur.getEnd()].append(Edge(cur.getEnd(), cur.getStart(), cur.getTime()))
 
             if (edge < 2):
                 reps[self.find(reps, cur.getStart())] = self.find(reps, cur.getEnd())
-
         return output
-
-    def get_total_time(self, dict):
+    
+    def get_total_time(self):
         total = 0
-        for edge in self.optimize_path(dict):
-            total += edge.getWeight()
+        for edge in self.optimize_path():
+            total += edge.getTime()
         return total
 
-    def optimize_path(self, dict):
-
+    def optimize_path(self):
         output = []
         curVertex = None
         prevVertex = None
-        for node in dict:
-            if (len(dict[node]) == 1):
-                output.append(dict[node][0])
+        for node in self.min_tree:
+            if (len(self.min_tree[node]) == 1):
+                output.append(self.min_tree[node][0])
                 prevVertex = node
-                curVertex = dict[node][0].getEnd()
+                curVertex = self.min_tree[node][0].getEnd()
                 break
         
-        while(len(dict[curVertex]) == 2):
+        while(len(self.min_tree[curVertex]) == 2):
             index = 0
-            if (dict[curVertex][0].getEnd() == prevVertex):
+            if (self.min_tree[curVertex][0].getEnd() == prevVertex):
                 index = 1
             
-            output.append(dict[curVertex][index])
+            output.append(self.min_tree[curVertex][index])
             prevVertex = curVertex
-            curVertex = dict[curVertex][index].getEnd()
+            curVertex = self.min_tree[curVertex][index].getEnd()
 
         return output
+
+if __name__ == "__main__":
+    v1 = Vertex("Location 1", 5)
+    v2 = Vertex("Location 2", 15)
+    v3 = Vertex("Location 3", 20)
+    v4 = Vertex("Location 4", 3)
+    v5 = Vertex("Location 5", 7)
+    v6 = Vertex("Location 6", 8)
+    all_units = [v1, v2, v3, v4, v5, v6]
+    map_of_name = {node.getName(): node for node in all_units}
+    distance_matrix = {
+        v1: {v2: 1, v3: 4, v4: 1, v5: 3, v6: 6},
+        v2: {v3:3, v4: 2, v5: 2, v6: 7},
+        v3: {v4: 3, v5: 2, v6: 6},
+        v4: {v5: 1, v6: 6},
+        v5: {v6: 5},
+        v6: {}
+    }
+
+    print(Graph(all_units, distance_matrix).get_total_time())
